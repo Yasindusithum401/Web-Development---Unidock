c082240829f939c18f737c85a2f443f3088c5048
Yasindu Sithum <yasindusithumnimsara01@gmail.com>
2026-07-15 23:14:18 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+//login popup eka 
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modalOverlay = document.getElementById('modaloverlay');
@@ -5,7 +6,6 @@ const switchToSignUp = document.getElementById('switchToSignUp');
 const switchToLogIn = document.getElementById('switchToLogIn');
 const loginFormContainer = document.getElementById('loginFormContainer');
 const signupFormContainer = document.getElementById('signupFormContainer');
-
 if(openModalBtn) {
     openModalBtn.addEventListener('click', (event) => {
         event.preventDefault();
@@ -41,3 +41,34 @@ if(switchToLogIn) {
         loginFormContainer.classList.add('active');
     });
 }
+//home page text animation eka
+const words = [
+    "Share, Discover and Access Quality Study Materials Easily.Learn Together, Grow Together."
+];
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+const text = document.getElementById("text");
+function typing(){
+    let current = words[wordIndex];
+    if(!deleting){
+        text.textContent = current.substring(0,charIndex + 1);
+        charIndex++;
+        if(charIndex === current.length){
+            deleting = true;
+            setTimeout(typing,1000);
+            return;
+        }
+    }
+    else{
+        text.textContent = current.substring(0,charIndex - 1);
+        charIndex--;
+        if(charIndex === 0){
+            deleting = false;
+            wordIndex++;
+            if(wordIndex === words.length){wordIndex = 0 }
+        }
+    }
+    setTimeout(typing,67);
+}
+typing();
