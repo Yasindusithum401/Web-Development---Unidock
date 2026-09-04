119bb2b47a6fc76350d5d28f905ae97240957d73
Yasindu Sithum <yasindusithumnimsara01@gmail.com>
2026-09-05 01:26:43 +0530

    Create db.php

---
+++ includes/db.php
@@ -0,0 +1,24 @@
+<?php
+
+$dbHost = 'localhost';
+$dbName = 'unidock';   
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$dbHost};dbname={$dbName};charset=utf8mb4",
+        $dbUser,
+        $dbPass,
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ]
+    );
+} catch (PDOException $e) {
+    http_response_code(500);
+    die(
+        '<p style="font-size:13px;color:#777;">Error: ' . htmlspecialchars($e->getMessage()) . '</p>'
+    );
+}
